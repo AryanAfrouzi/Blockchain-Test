20e4a909a47fc95e41feffcf2aea5dfcc4279feb
AryanAfrouzi <aryanafrouzi@gmail.com>
2018-09-09 20:31:33 -0400

    First Commit
    First Commit to Github

---
+++ cmake-build-debug/CMakeFiles/aryan_blockchain.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aryan_blockchain.dir/blockchain.c.obj"
+  "aryan_blockchain.pdb"
+  "aryan_blockchain.exe"
+  "aryan_blockchain.exe.manifest"
+  "libaryan_blockchain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aryan_blockchain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/simple_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_example.dir/blockchain.c.obj"
+  "simple_example.pdb"
+  "simple_example.exe"
+  "simple_example.exe.manifest"
+  "libsimple_example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
